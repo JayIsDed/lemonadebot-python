651a0dd2bc98df05412ac924d64b3b8729c6540e
jayisded0517 <jayisded0517@gmail.com>
2018-06-07 13:43:00 -0400

    Triggering kevin v2

--- lemonadebot.py
+++ lemonadebot.py
@@ -29,7 +29,7 @@ async def on_message(message):
 
     if message.content.upper() == "KEVIN":
         emb1 = discord.Embed(title='NSFW', colour=0x00ff00)
-        emb1.add_field(name='NSFW', description='https://www.pornhub.com')
+        emb1.add_field(name='NSFW', value='https://www.pornhub.com')
         await bot.send_message(message.channel, embed.emb1)
 
     if message.content.upper() == "PADDLE":
